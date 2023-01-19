064534a4bb931887055e6053846b443987a231f8
Gerrard Rose <gerrard.rose@rebura.com>
2023-01-19 17:36:35 +0000

    actions update

---
+++ env_vars/production.tfvars
@@ -0,0 +1,7 @@
+aws_region="eu-west-1"
+
+vpc_cidr="10.30.0.0/16"
+
+azs=["eu-west-1a", "eu-west-1b", "eu-west-1c"]
+private_subnets=["10.30.1.0/24", "10.30.2.0/24", "10.30.3.0/24"]
+public_subnets=["10.30.101.0/24", "10.30.102.0/24", "10.30.103.0/24"]
---
+++ vpc.tf
@@ -0,0 +1,16 @@
+# Using the module from https://github.com/terraform-aws-modules/terraform-aws-vpc
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "Org1 VPC"
+  cidr = var.vpc_cidr
+
+  azs             = var.azs
+  private_subnets = var.private_subnets
+  public_subnets  = var.public_subnets
+
+  enable_nat_gateway = false
+  enable_vpn_gateway = false
+  enable_s3_endpoint = false
+
+}
