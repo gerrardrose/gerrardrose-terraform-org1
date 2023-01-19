7bc0e428af0c70d817b7be766a8e827df6447199
Gerrard Rose <gerrard.rose@rebura.com>
2023-01-19 16:56:27 +0000

    actions update

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
-resource "aws_ssm_parameter" "foobar40" {
-  name  = "foobar40"
+resource "aws_ssm_parameter" "foobar41" {
+  name  = "foobar41"
   type  = "String"
-  value = "bar40"
+  value = "bar41"
 }
