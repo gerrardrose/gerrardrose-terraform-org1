8dd50eb3c856d80e2f305036dbda28a6cb3b8195
Gerrard Rose <gerrard.rose@rebura.com>
2023-01-20 12:37:48 +0000

    add vpc 2

--- vpc.tf
+++ vpc.tf
@@ -3,10 +3,18 @@ module "vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
   name = "Org1 VPC"
-  cidr = var.vpc_cidr
+  cidr = "10.0.0.0/16"
 
-  azs             = var.azs
-  private_subnets = var.private_subnets
-  public_subnets  = var.public_subnets
+  azs             = ["eu-west-1a", "eu-west-1b", "eu-west-1c"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+
+  enable_nat_gateway = false
+  enable_vpn_gateway = false
+
+  tags = {
+    Terraform = "true"
+    Environment = "dev"
+  }
 
 }
