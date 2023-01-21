e299977b9d3bf221d6ccbaf7ef7a1f5b76a4119e
Gerrard Rose <gerrard.rose@rebura.com>
2023-01-21 22:33:21 +0000

    add vpc 44

---
+++ management/ec2.tf
@@ -0,0 +1,8 @@
+module "ec2" {
+  source	= "../modules/ec2"
+#  aws_region	= var.aws_region
+#  vpc_cidr      = var.vpc_cidr
+#  azs             = var.azs
+#  private_subnets = var.private_subnets
+#  public_subnets  = var.public_subnets
+}
